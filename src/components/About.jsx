630fc08eb3dd75e74859fdea0049c371a2dc532b
Shreyas Parab <shreyasparab264@gmail.com>
2025-12-24 13:17:58 +0530

    testing change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import profile from '../assets/img/profile.jpg'
 
 const About = () => {
     return (
         <section id='about'>
-            <img src='/src/assets/img/profile.jpg' alt='profile picture' />
+            <img src={profile} alt='profile picture' />
             <h1>Shreyas Vilas Parab</h1>
             <p>I am a Full Stack Developer who enjoys building practical, intuitive, and reliable applications. I take pride in 
 solving complex challenges, ensuring accuracy in my work, and collaborating with others to create effective 
